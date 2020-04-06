c888b3ef177cd546c18b2124370f8b679447c7c1
Shoaib Jakvani <sjakva3@uic.edu>
2020-04-06 03:50:52 +0000

    adding final fixes v5

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", showCoffees)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/serviceWorker.js').then(function(registration) {
+      .register('./serviceWorker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
